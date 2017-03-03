71d327c6a52375c348fee9dee089a8266c58a6ae
Jordan Penard <jordan038@gmail.com>
2017-03-03 19:41:41 +0000

    Runtime optimisation. Code splited up in functions for part of the processing.

--- un_lzw.c
+++ un_lzw.c
@@ -5,9 +5,60 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#define max(x,y) (x ^ ((x ^ y) & -(x < y)))
+
+
 #define DICO_SIZE 4096
 #define DICO_INIT_END 256
-#define DICO_STRING_INIT 4
+#define DICO_STRING_INIT 256
+
+#define OUTPUT_BUFFER 40960
+
+
+FILE *f_out;
+uint8_t buff_out[OUTPUT_BUFFER];
+uint16_t buff_pointer_out = 0;
+
+uint8_t* dictionary[DICO_SIZE];
+uint32_t dico_alloc[DICO_SIZE];
+uint32_t dico_data_size[DICO_SIZE];
+uint16_t dico_i = DICO_INIT_END;
+
+
+void add_to_dico(uint16_t last_string_index, uint16_t x) {
+
+    if (dico_i >= DICO_SIZE)
+       dico_i = DICO_INIT_END;
+
+    // new_string = last_string & 1 char of current_string
+    int new_string_len = dico_data_size[last_string_index] + 1;
+
+    if (new_string_len > dico_alloc[dico_i]) {
+        dico_alloc[dico_i] = max(dico_alloc[dico_i] * 2, new_string_len);
+                    
+        free(dictionary[dico_i]);
+        dictionary[dico_i] = malloc(dico_alloc[dico_i]);
+    }
+
+    // add new_string to the dictionary
+    memcpy(dictionary[dico_i], dictionary[last_string_index], new_string_len-1);
+    dictionary[dico_i][new_string_len-1] = dictionary[x][0];
+    dico_data_size[dico_i] = new_string_len;
+
+    dico_i++;
+}
+
+void output(uint8_t* data, uint32_t data_size) {
+
+    if (data_size + buff_pointer_out > OUTPUT_BUFFER) {
+        fwrite(buff_out, 1, buff_pointer_out, f_out);
+        buff_pointer_out = 0;
+    }
+
+    memcpy(&buff_out[buff_pointer_out], data, data_size);
+    buff_pointer_out += data_size;
+}
+
 
 
 int main(int argc, char** argv) {
@@ -15,11 +66,9 @@ int main(int argc, char** argv) {
     FILE *fileptr;
     char *buffer;
     long filelen;
-
-    char* dictionary[DICO_SIZE];
-    uint32_t dico_alloc[DICO_SIZE];
     
-        if (argc < 2) {
+
+    if (argc < 2) {
         printf("error, no input file given\n");
         return 1;
     }
@@ -28,9 +77,9 @@ int main(int argc, char** argv) {
 
     // Init dictionary
     for(i = 0; i < DICO_INIT_END; i++) {
-        dictionary[i] = malloc(2);
+        dictionary[i] = malloc(1);
         dictionary[i][0] = i;
-        dictionary[i][1] = 0;
+        dico_data_size[i] = 1;
     }
 
     for(i = DICO_INIT_END; i < DICO_SIZE; i++) {
@@ -48,12 +97,9 @@ int main(int argc, char** argv) {
     buffer = (char *)malloc((filelen+1)*sizeof(char)); // Enough memory for file + \0
     fread(buffer, filelen, 1, fileptr); // Read in the entire file
     fclose(fileptr); // Close the file
-    
-    FILE *output = fopen("out", "w");
 
-    uint16_t dico_i = 256;
+    f_out = fopen("out_v2", "wb");
 
-    char* last_string = 0;
 
     // data is comming as follow (x and y beeing 2 different data from 3 input bytes)
     // xxxx xxxx  xxxx yyyy  yyyy yyyy
@@ -66,77 +112,57 @@ int main(int argc, char** argv) {
     //    1          2
     //
 
+    bool first_run = true;
+    uint16_t last_string_index = 0;
     
     char* data = buffer;
-    do {
+    while(data < (buffer + filelen)) {
         uint8_t b1 = (uint8_t)*data;
         uint8_t b2 = (uint8_t)*(data+1);
-        uint8_t b3 = 0;
-        uint16_t x, y;
-
-        bool three_symboles = false;
+        uint16_t x;
         
         // If there is at least 3 bytes left
         if (filelen - (data - buffer) >= 3) {
-            b3 = (uint8_t)*(data+2);
+            uint8_t b3 = (uint8_t)*(data+2);
+            
             x = ( b1 << 4) | ( b2 >> 4 ) ;
-            y = ( (b2 & 0x0F) << 8) | b3 ;
-            three_symboles = true;
-        } else {            
-            x = (b1 << 8) | b2 ;
-            y = 0;
-        }
-
-        // If there is at least 3 bytes left, loop twice
-        // Otherwise just exec this once
-        for(i = 0; i < (three_symboles ? 2 : 1); i++) {
-                
+            uint16_t y = ( (b2 & 0x0F) << 8) | b3 ;
+            
             // Not first iteration
-            if (last_string != 0) {
-
-                if (dico_i >= DICO_SIZE)
-                   dico_i = DICO_INIT_END;
-
-                // new_string = last_string & 1 char of current_string
-                int new_string_len = strlen(last_string) + 2;
-
-                if (new_string_len > dico_alloc[dico_i]) {
-                    do {
-                        dico_alloc[dico_i] = dico_alloc[dico_i] * 2;
-                    } while(new_string_len > dico_alloc[dico_i]);
-                        
-                    free(dictionary[dico_i]);
-                    dictionary[dico_i] = malloc(dico_alloc[dico_i]);
-                }
-
-                // Dodgy fix for the end of the big file
-                if (x > dico_i) {
-                    printf("Warning, b1:0x%x b2:0x%x b3:0x%x dico_i:%d x:%d\nx >> 4\n", b1, b2, b3, dico_i, x);
-                    x = x >> 4;
-                }
-
-                // add new_string to the dictionary
-                strcpy(dictionary[dico_i], last_string);
-                dictionary[dico_i][new_string_len-2] = dictionary[x][0];
-                dictionary[dico_i][new_string_len-1] = '\0';
-
-                dico_i++;
+            if (!first_run) {
+                add_to_dico(last_string_index, x);
             }
+            first_run = false;
 
             // Print the decompressed value for the current symbole
-            fprintf(output, "%s", dictionary[x]);
+            output(dictionary[x], dico_data_size[x]);
             
             // Prepare for the next iteration
-            last_string = dictionary[x];
-
-            // In case we loop twice, the next symbole will be in x ready to be processed
+            last_string_index = x;
             x = y;
+
+            // Consumed 3 bytes
+            data += 3;
+                        
+        } else {            
+            x = (b1 << 8) | b2 ;
+
+            // Consumed 2 bytes
+            data += 2;
         }
-        
-        // Consumed 2 or 3 bytes depending on three_symboles
-        data += (three_symboles ? 3 : 2);
-                    
-    } while(data < (buffer + filelen));
+
+        add_to_dico(last_string_index, x);
+
+        // Print the decompressed value for the current symbole
+        output(dictionary[x], dico_data_size[x]);
+            
+        // Prepare for the next iteration
+        last_string_index = x;                    
+    }
+
+
+    fwrite(buff_out, 1, buff_pointer_out, f_out);
+    fclose(f_out); // Close the file
 
     for(i = 0; i < DICO_SIZE; i++) {
         free(dictionary[i]);
