4fc53d2d6f38ee80cc6b22cc66f3cd62c53fb23f
Jordan Penard <jordan038@gmail.com>
2017-03-04 19:52:42 +0000

    Fixing some bugs on the compressor

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
+all: compressor decompressor
+
 compressor:
 	gcc -O2 -g lzw_compressor.c -o lzw_compressor
 
 decompressor:
 	gcc -O2 -g lzw_decompressor.c -o lzw_decompressor
 
-all: compressor decompressor
+clean:
+	rm -rf lzw_compressor.dSYM/ lzw_decompressor.dSYM/ lzw_compressor lzw_decompressor
--- lzw_compressor.c
+++ lzw_compressor.c
@@ -55,29 +55,39 @@ void output(uint16_t data) {
 }
 
 
+void reset_dictionary(node_t *root_node) {
+
+    uint16_t i = 0;
+
+    memset(&root_node[1], 0, sizeof(node_t) * DICO_SIZE);
+
+    for(i = 0; i < DICO_INIT_END; i++) {
+        root_node[i+1].index = i;
+    }
+}
+
+
 int main(int argc, char** argv) {    
 
     uint16_t i = 0;
 
     // Are building a tree here, so we need nb items + 1 for the root
-    node_t root_node[DICO_SIZE+1];
+    node_t *root_node = malloc(sizeof(node_t) * (DICO_SIZE+1)); 
     node_t *current_node = root_node;
+    for(i = 0; i < DICO_INIT_END; i++) {
+        root_node[0].next[i] = &root_node[i+1];
+    }    
+    reset_dictionary(current_node);
 
     // Next index to be writen
     uint16_t next_index = DICO_INIT_END+1;
-    
-    // Init dictionary
-    memset(root_node, 0, sizeof(node_t) * (DICO_SIZE+1));
-    for(i = 0; i < DICO_INIT_END; i++) {
-        root_node[0].next[i+1] = &root_node[i+1];
-        root_node[i+1].index = i;
-    }
- 
+     
     // Input buffer
     uint8_t buff_in[INPUT_BUFFER];
 
     // This is possitive to enter the while for the 1st tine
     uint16_t data_len = 1;
+
     
     while(data_len) {
 
@@ -92,26 +102,30 @@ int main(int argc, char** argv) {
                 // Print the current index
                 output(current_node->index);
 
-                // If dictionary full, reset
-                if (next_index > DICO_SIZE) {
-                    next_index = DICO_INIT_END+1;
-                    memset(root_node, 0, sizeof(node_t) * (DICO_SIZE+1));
-                }
-
                 // Add current char to the end of found string
                 current_node->next[*data] = &root_node[next_index];
-                root_node[next_index].index = next_index;
+                root_node[next_index].index = next_index - 1;
                 next_index++;
-                
+
+                // If dictionary full, reset
+                if (next_index > DICO_SIZE) {
+                    reset_dictionary(root_node);
+                    next_index = DICO_INIT_END+1;
+                }
+
                 // Re-explore tree from the top
-                current_node = &root_node[*data];
+                current_node = root_node;
                 
-            } else {
-                // Progress through the tree as we found a matching string
-                current_node = current_node->next[*data];
             }
+            // Progress through the tree as we found a matching string
+            current_node = current_node->next[*data];
+
+            data++;
         }
     }
+
+    // Print the last char
+    output(current_node->index);
 
     // If we are not alligned on a full byte from the previous write, pad as follow
     // [0] : 0 0 0 0 x11 x10 x9 x8
@@ -119,7 +133,8 @@ int main(int argc, char** argv) {
     if (mid_byte_write) {
         buff_out[buff_pointer_out] = (buff_out[buff_pointer_out-1] << 4)
                                      | (buff_out[buff_pointer_out] >> 4); 
-        buff_out[buff_pointer_out-1] = buff_out[buff_pointer_out-1] >> 4;         
+        buff_out[buff_pointer_out-1] = buff_out[buff_pointer_out-1] >> 4;
+        buff_pointer_out++;
     }
 
     // Write the content of the buff to the output
